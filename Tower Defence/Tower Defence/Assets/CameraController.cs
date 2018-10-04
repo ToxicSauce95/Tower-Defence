1d3d796da04ce74c514bdee02fc274cf2e098862
ToxicSauce95 <43681879+ToxicSauce95@users.noreply.github.com>
2018-10-04 11:13:35 -0700

    Created AI

--- Tower Defence/Tower Defence/Assets/CameraController.cs
+++ Tower Defence/Tower Defence/Assets/CameraController.cs
@@ -6,37 +6,45 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class CameraController : MonoBehaviour
 {
+    #region Settings
+
+    #region LookSettings
+
     private bool a = false;
     private bool locked = false;
-	public float mainSpeed = 100.0f; //regular speed
-    public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
-    public float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    public float camSens = 0.25f; //How sensitive it with mouse
+    private float NormalSpeed = 50.0f; //regular speed
+    private float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    private float SprintSpeed = 100.0f; //Maximum speed when holdin gshift
+    public float MouseSensitivity = 0.25f; //How sensitive it with mouse
     Vector3 lastMouse = new Vector3(); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun= 1.0f;
-    
+
+    #endregion
+    #region ZoomSettings
+
     private float minFov = 15f;
     private float maxFov = 90f;
-    private float sensitivity = 10f;
+    public float ZoomSensitivity = 10f;
     private float fov = 60f;
+
+#endregion
+
+    #endregion
     
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
     }
     
-    
     void Update () {
-        
+
         fov = Camera.main.fieldOfView;
-        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * ZoomSensitivity;
         fov = Mathf.Clamp(fov, minFov, maxFov);
         Camera.main.fieldOfView = fov;
-        
-        
+
         var x = CrossPlatformInputManager.GetAxis("Mouse X");
         var y = CrossPlatformInputManager.GetAxis("Mouse Y");
-        
         
         if (Input.GetMouseButton(1))
         {
@@ -57,35 +65,26 @@ public class CameraController : MonoBehaviour
         if (locked || a)
         {
             lastMouse = Input.mousePosition - lastMouse;
-            lastMouse = new Vector3(-x * camSens, y * camSens, 0);
+            lastMouse = new Vector3(-x * MouseSensitivity, y * MouseSensitivity, 0);
             lastMouse = new Vector3(transform.eulerAngles.x - y, transform.eulerAngles.y + x, 0);
             transform.eulerAngles = lastMouse;
         }
         
-            
-            
-        
-        
-        //Mouse  camera angle done.  
-        
-        
-        //Keyboard commands
-        float f = 0.0f;
         Vector3 p = GetBaseInput();
         if (Input.GetKey (KeyCode.LeftShift)){
             totalRun += Time.deltaTime;
             p  = p * totalRun * shiftAdd;
-            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+            p.x = Mathf.Clamp(p.x, -SprintSpeed, SprintSpeed);
+            p.y = Mathf.Clamp(p.y, -SprintSpeed, SprintSpeed);
+            p.z = Mathf.Clamp(p.z, -SprintSpeed, SprintSpeed);
         }
         else{
             totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
+            p = p * NormalSpeed;
         }
        
         p = p * Time.deltaTime;
-       Vector3 newPosition = transform.position;
+        Vector3 newPosition = transform.position;
         if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
             transform.Translate(p);
             newPosition.x = transform.position.x;
@@ -95,10 +94,11 @@ public class CameraController : MonoBehaviour
         else{
             transform.Translate(p);
         }
-       
-    }
-     
-    private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
+        
+    }   
+    
+    private Vector3 GetBaseInput() {
+        
         Vector3 p_Velocity = new Vector3();
         if (Input.GetKey (KeyCode.W)){
             p_Velocity += new Vector3(0, 0 , 1);
---
+++ Tower Defence/Tower Defence/Assets/EnemyAIScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIScript : MonoBehaviour
+{
+
+	public Rigidbody Enemy;
+	public float MovementSpeed = 10.0f;
+	Vector3 lastEnemy = new Vector3();
+	
+	void Start ()
+	{
+		GetComponent<Rigidbody>();		 
+	}
+	
+	
+	void FixedUpdate()
+	{
+		transform.position += transform.forward * Time.deltaTime * MovementSpeed;
+		
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast(transform.position, fwd, 10))
+		{
+			transform.Rotate(new Vector3(0,90,0));
+
+		}
+			
+	}
+}

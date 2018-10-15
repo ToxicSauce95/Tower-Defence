84441322fad8c9637086644b824d1b41f60269f8
ToxicSauce95 <43681879+ToxicSauce95@users.noreply.github.com>
2018-10-15 11:39:38 -0700

    change enemy movement

--- Tower Defence/Tower Defence/Assets/EnemyAIScript.cs
+++ Tower Defence/Tower Defence/Assets/EnemyAIScript.cs
@@ -1,61 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAIScript : MonoBehaviour
 {
 
-	public float MovementSpeed = 10.0f;
-	private float random;
-
-	void Start()
-	{
-		GetComponent<Rigidbody>();
-	}
-
-	private void OnTriggerEnter(Collider other)
-	{
-		
-		
-		
-		if (other.gameObject.CompareTag("Enemy"))
-		{
-		
-		}
-		else
-		{
-		
-
-			Vector3 fwd = transform.TransformDirection(Vector3.forward);
-			Vector3 left = transform.TransformDirection(Vector3.left);
-			Vector3 right = transform.TransformDirection(Vector3.right);
-
-			if (Physics.Raycast(transform.position, fwd, 5))
-			{
-				if (Physics.Raycast(transform.position, left, 5))
-				{
-					transform.Rotate(new Vector3(0, 90, 0));
-				}
-				else if (Physics.Raycast(transform.position, right, 5))
-				{
-					transform.Rotate(new Vector3(0, -90, 0));
-				}
-				else
-				{
-					random = Random.value;
-					if (random <= .5)
-						transform.Rotate(new Vector3(0, 90, 0));
-					if (random > .5)
-						transform.Rotate(new Vector3(0, -90, 0));
-
-				}
-			}
-		}
-	}
-
-	private void Update()
-	{
-		transform.position += transform.forward * Time.deltaTime * MovementSpeed;
-		
+	public Transform goal;
+       
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
 	}
 }

6de7def878c9603d69d0f2dc0bfc8e900181ca78
ToxicSauce95 <43681879+ToxicSauce95@users.noreply.github.com>
2018-10-17 11:13:49 -0700

    AI

--- Tower Defence/Tower Defence/Assets/EnemyAIScript.cs
+++ Tower Defence/Tower Defence/Assets/EnemyAIScript.cs
@@ -10,7 +10,9 @@ public class EnemyAIScript : MonoBehaviour
 	public Transform goal;
        
 	void Start () {
+
 		NavMeshAgent agent = GetComponent<NavMeshAgent>();
 		agent.destination = goal.position; 
 	}
 }
+//
